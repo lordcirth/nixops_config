d3b32cb4079d554537d6cc71bed037f19acee67c
Nathan Fish <lordcirth@gmail.com>
2021-11-25 15:34:07 -0500

    mirror Jargon File

--- lighttpd.nix
+++ lighttpd.nix
@@ -1,7 +1,12 @@
 { config, pkgs, nodes, ... }: {
-  services.lighttpd = {
+  services.lighttpd = let
+    datadir = fetchTarball {
+      url = "http://www.catb.org/jargon/jargon-4.4.7.tar.gz";
+      sha256 = "0vfk4908lhgzbqgz3n4km05wnr6y5v52i1ipybrsa93069yiwdi7";
+    };
+  in {
     enable = true;
-    # document-root = fetchGit {};
+    document-root = "${datadir}/html";
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
